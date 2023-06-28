2b0c2216bc3c747d72b0d91398bf6e77257104ab
Pierre Barjon <barjon.pierre@gmail.com>
2023-06-28 16:21:52 +0200

    Add simple test project

---
+++ src/Tests/FloatableWindowSamples/MainPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FloatableWindowSamples
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new FloatableWindow
+            {
+                ParentLayoutRoot = LayoutRoot,
+                Content = new Border
+                {
+                    Width = 200,
+                    Height = 200,
+                    Background = new SolidColorBrush(Colors.Red),
+                    Child = new TextBlock
+                    {
+                        Text = "This is a non-modal FloatableWindow",
+                        FontSize = 20,
+                        TextAlignment = TextAlignment.Center,
+                        TextWrapping = TextWrapping.Wrap,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                    },
+                },
+            };
+
+            window.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var window = new FloatableWindow
+            {
+                ParentLayoutRoot = LayoutRoot,
+                Content = new Border
+                {
+                    Width = 200,
+                    Height = 200,
+                    Background = new SolidColorBrush(Colors.Yellow),
+                    Child = new TextBlock
+                    {
+                        Text = "This is a modal FloatableWindow",
+                        FontSize = 20,
+                        TextAlignment = TextAlignment.Center,
+                        TextWrapping = TextWrapping.Wrap,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                    },
+                },
+            };
+
+            window.ShowDialog();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            double.TryParse(hOffsetTB.Text, out double hOffset);
+            double.TryParse(vOffsetTB.Text, out double vOffset);
+
+            var window = new FloatableWindow
+            {
+                ParentLayoutRoot = LayoutRoot,
+                Content = new Border
+                {
+                    Width = 200,
+                    Height = 200,
+                    Background = new SolidColorBrush(Colors.LightGreen),
+                    Child = new TextBlock
+                    {
+                        Text = "This is a non-modal FloatableWindow",
+                        FontSize = 20,
+                        TextAlignment = TextAlignment.Center,
+                        TextWrapping = TextWrapping.Wrap,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                    },
+                },
+            };
+
+            window.Show(hOffset, vOffset);
+        }
+    }
+}
